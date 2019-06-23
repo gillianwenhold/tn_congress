3c691c7afbdd8661bae80fa2942945576f9613cd
gillianwenhold <gwenhold@mac.com>
2019-06-23 14:16:03 -0400

    created gem

---
+++ Gemfile
@@ -0,0 +1,4 @@
+source "https://rubygems.org"
+
+# Specify your gem's dependencies in tn_congress.gemspec
+gemspec
---
+++ lib/tn_congress.rb
@@ -0,0 +1,6 @@
+require "tn_congress/version"
+
+module TnCongress
+  class Error < StandardError; end
+  # Your code goes here...
+end
